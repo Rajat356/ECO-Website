7f7f56f25ccff91e53257b4c4df8bbe8ac7d6344
Harshit Gupta <guptaharshit7718@gmail.com>
2024-04-10 14:52:53 +0530

    Updated Backend

--- index.js
+++ index.js
@@ -18,9 +18,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/ECO", { useNewUrlParser: true });
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
+    unique: true,
+    required: true,
   },
   password: {
     type: String,
+    required: true,
   },
 });
 
