19ab0a0c54ba61a1a0caa18d5585873a3290f826
Harshit Gupta <guptaharshit7718@gmail.com>
2024-04-13 23:29:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,27 @@ let userPassword="";
 app.set('view engine', 'ejs');
 
 // mongoose.connect("mongodb://127.0.0.1:27017/ECO", { useNewUrlParser: true });
-mongoose.connect("mongodb://127.0.0.1/ECO", { useNewUrlParser: true, useUnifiedTopology: true,family: 4});
+const uri = 'mongodb://localhost:27017/test';
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+
+const connectWithDB = () => {
+    mongoose.connect(uri, options, (err, db) => {
+      if (err) console.error(err);
+      else console.log("database connection")
+    })
+}
+
+connectWithDB()
 
 const userSchema = new mongoose.Schema({
   email: {
